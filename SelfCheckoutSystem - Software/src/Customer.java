6222f7550482cf30cedaa3e62e683dfbcd3b8718
g-heemeryck <grace.heemeryck1@ucalgary.ca>
2021-03-17 16:37:21 -0600

    Update Customer.java

--- SelfCheckoutSystem - Software/src/Customer.java
+++ SelfCheckoutSystem - Software/src/Customer.java
@@ -17,12 +17,12 @@ public class Customer {
    
     
     public Customer () {
-    	// create the needed selfcheckoutstation for all t ests
+    	// create the needed selfcheckoutstation for all tests
     	Currency currency = Currency.getInstance("CAD");
     	int[] bankDenom = {5, 10, 20, 50, 100};
     	BigDecimal[] coinDenom = {new BigDecimal(0.05), new BigDecimal(0.10), new BigDecimal(0.25), new BigDecimal(1.00), new BigDecimal(2.00)};
-    	// Self checkout station: so far scale maximum weight + sensitivity are 100 and 1, this can be changed according to scan stuff, this is just an example.
-    	checkout = new SelfCheckoutStation(currency, bankDenom, coinDenom, 100, 1);
+    	// Self checkout station: so far scale maximum weight + sensitivity are 23000g and 10g, this can be changed as necessary, this is just an example.
+    	checkout = new SelfCheckoutStation(currency, bankDenom, coinDenom, 23000, 10);
     }
 
 //indicating that payment has started with coins
