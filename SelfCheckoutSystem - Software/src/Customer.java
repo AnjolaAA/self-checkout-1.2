6f6849516cc191cc88f0ccbf7fdc51a23eb63291
g-heemeryck <grace.heemeryck1@ucalgary.ca>
2021-03-17 15:41:59 -0600

    Update Customer.java
    Modified customer to make it easier to test, mainly through creating a selfcheckoutstation that we can use to mimic the actual self-checkout station hardware.

--- SelfCheckoutSystem - Software/src/Customer.java
+++ SelfCheckoutSystem - Software/src/Customer.java
@@ -6,24 +6,50 @@
 public class Customer {
 
     //should we have a class for this? Keep track of item, barcode, and running total?
-    BigDecimal runningTotal;
-    List<BarcodedItem> itemTotal;
-    BarcodeScanner scanner;
-
-    public static void main(String args[]){
-        System.out.println("Hello World");
+    public BigDecimal runningTotal;
+    //List<BarcodedItem> itemTotal;
+   // BarcodeScanner scanner;
+    public SelfCheckoutStation checkout;
+    private PayWithCoin payment;
+   
+    
+    public Customer () {
+    	// create the needed selfcheckoutstation for all t ests
+    	Currency currency = Currency.getInstance("CAD");
+    	int[] bankDenom = {5, 10, 20, 50, 100};
+    	BigDecimal[] coinDenom = {new BigDecimal(0.05), new BigDecimal(0.10), new BigDecimal(0.25), new BigDecimal(1.00), new BigDecimal(2.00)};
+    	// Self checkout station: so far scale maximum weight + sensitivity are 100 and 1, this can be changed according to scan stuff, this is just an example.
+    	checkout = new SelfCheckoutStation(currency, bankDenom, coinDenom, 100, 1);
     }
+
+//indicating that payment has started with coins
+   public void startCoinPay(){
+       payment = new PayWithCoin(this.runningTotal);
+       //register as a listener
+       checkout.coinValidator.register(payment);
+
+    }
+  
+  //paying with coins
+  	public void pay (Coin coin) throws DisabledException {
+  		// mimics customer paying with a coin
+  		checkout.coinSlot.accept(coin);
+  	}
+  
+   //ending payment with coins, checks to see if it's valid.
+  	public boolean endCoinPay() {
+  		if (payment.finishPaying()) {
+  			System.out.println("Successfully end transaction");
+  			return true;
+  		}
+  		return false;
+  	}
 
     public void addItem(BarcodedItem item){
         scanner.scan(item);
     }
 
     public void placeItemInBagging(){
-
-    }
-
-    public void pay(Coin coin){
-        payWithCoin(this.runningTotal);
 
     }
 
