f51c786976539d1c70dcb8f7157502a7215085b0
AnjolaAA <45274850+AnjolaAA@users.noreply.github.com>
2021-03-17 06:38:01 -0600

    Added stubs

---
+++ SelfCheckoutSystem - Software - Test/src/BarcodeScannerListenerStub.java
@@ -0,0 +1,22 @@
+//Import statements
+//Import the packages
+
+public class BarcodeScannerListenerStub implements BarcodeScannerListener {
+
+    @Override
+    public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
+        // TODO Auto-generated method stub
+    }
+
+}
