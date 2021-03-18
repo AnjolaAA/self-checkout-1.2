8d15c83535aed1d3347c2b23b67b0a5bf579b01e
KeatonBuddy <47367626+KeatonBuddy@users.noreply.github.com>
2021-03-17 21:01:40 -0600

    Add files via upload

---
+++ SelfCheckoutSystem - Software/src/BagArea.java
@@ -0,0 +1,46 @@
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.ElectronicScale;
+import org.lsmr.selfcheckout.devices.listeners.AbstractDeviceListener;
+import org.lsmr.selfcheckout.devices.listeners.ElectronicScaleListener;
+
+public class BagArea implements ElectronicScaleListener{
+	private double totalWeight;
+	private boolean overloaded;
+	private boolean status;
+
+	@Override
+	public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+		// TODO Auto-generated method stub
+		status = true;
+		System.out.println("Device is enabled.");
+	}
+
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+		// TODO Auto-generated method stub
+		status = false;
+		System.out.println("Device is disabled.");
+	}
+
+	@Override
+	public void weightChanged(ElectronicScale scale, double weightInGrams) {
+		// TODO Auto-generated method stub
+		totalWeight = weightInGrams;
+	}
+
+	@Override
+	public void overload(ElectronicScale scale) {
+		// TODO Auto-generated method stub
+		overloaded = true;
+		System.out.println("Scale is Overloaded, Please remove an Item.");
+		
+	}
+
+	@Override
+	public void outOfOverload(ElectronicScale scale) {
+		// TODO Auto-generated method stub
+		overloaded = false;
+		System.out.println("Device is no longer Overloaded.");
+	}
+
+}
